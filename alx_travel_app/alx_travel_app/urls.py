0076f6143c0feee37ea135ae75cda26716c9ac69
stephen842 <ugotachisomstephen@gmail.com>
2025-11-28 02:52:30 +0100

    Built API views to manage listings and bookings, and ensure the endpoints are documented with Swagger.

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -32,5 +32,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('listings.urls')),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
